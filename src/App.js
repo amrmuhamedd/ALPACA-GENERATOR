4f23465abe8c90293a19e310556eaab226287787
amrmohamed <am1066903@gmail.com>
2021-12-31 20:43:25 +0200

    upload project to github

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Alpaca from "./components/Alpaca";
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Header />
+        <Alpaca />
+      </Layout>
+    </>
+  );
+}
+
+export default App;
